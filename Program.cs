c81a44691ff4659b2fcd26c3a89dd07a1aca084a
justKhoi1249 <khoipham1249@gmail.com>
2024-06-13 16:29:15 +0700

    bai moi my name is

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("hello hello hello");
 int myVariable = 10;
 string myName = "PHAMANHKHOI";
-Console.WriteLine("TEN TOI LA: {0}", myName);
+Console.WriteLine("MY NAME IS: {0}", myName);
